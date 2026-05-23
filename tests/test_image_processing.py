c6d57aef6467908e045c53398abfd6da21e1bb25
kagabrielco-netizen <kagabrielco@ucvvirtual.edu.pe>
2026-05-23 11:48:04 -0500

    Create temporary output directory in test
    Add temporary directory creation for test outputs.

--- tests/test_image_processing.py
+++ tests/test_image_processing.py
@@ -1,12 +1,15 @@
+from pathlib import Path
 from si_image_processing.pipelines.image_processing.nodes import process_image
 
 
 def test_process_image():
-
+    # Esto crea la carpeta temporal en GitHub si no existe
+    output_dir = Path("data/03_primary")
+    output_dir.mkdir(parents=True, exist_ok=True)
+    
     result = process_image(
         "data/01_raw/marte.jpg",
         "data/03_primary/test_output.jpg"
     )
 
     assert result is not None
-    
